647973bdbe60581fee4d6986f22fdfb712cf7b08
Rafael de Bem <debemrafael@gmail.com>
2024-02-28 00:53:08 -0300

    feat: update linux installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo cp dist/TuyaAPI.service /etc/systemd/system/
+sudo mkdir -p /www/tuya-api
+go build . && sudo cp tuya-api .env /www/tuya-api/
+
+sudo cp TuyaAPI.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable TuyaAPI
 sudo systemctl start TuyaAPI
